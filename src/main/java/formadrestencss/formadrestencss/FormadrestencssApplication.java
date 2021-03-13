a95864ae372f9cab4c29e27a02b118719ee531de
Jesper Stroebaek <74822634+JesperStroebaek@users.noreply.github.com>
2021-03-13 12:57:22 +0100

    1st

---
+++ src/main/java/formadrestencss/formadrestencss/FormadrestencssApplication.java
@@ -0,0 +1,13 @@
+package formadrestencss.formadrestencss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormadrestencssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormadrestencssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/formadrestencss/formadrestencss/FormadrestencssApplicationTests.java
@@ -0,0 +1,13 @@
+package formadrestencss.formadrestencss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormadrestencssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
